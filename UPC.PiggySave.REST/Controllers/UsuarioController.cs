6881e96cf778d13a33540391830a314f8c4e31c1
Christian Rojas <cgrojasr@live.com>
2022-01-24 10:18:53 -0500

    202200 Semana 3: Construcción de servicios REST para el objeto Usuario

---
+++ UPC.PiggySave.REST/Controllers/UsuarioController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UPC.PiggySave.BL;
+using UPC.PiggySave.DA;
+using UPC.PiggySave.REST.Models;
+
+namespace UPC.PiggySave.REST.Controllers
+{
+    [RoutePrefix("api/usuario")]
+    public class UsuarioController : ApiController
+    {
+        [HttpGet]
+        [Route("buscar/{idUsuario}")]
+        public IHttpActionResult Buscar(int idUsuario) {
+            var response = new Response<UsuarioModel>();
+            try
+            {
+                var objUsuarioBL = new UsuarioBL();
+                var objUsuario = objUsuarioBL.Buscar(idUsuario);
+                var objUsuarioModel = new UsuarioModel {
+                    idUsuario = objUsuario.idUsuario,
+                    nombre = objUsuario.nombre,
+                    apellido = objUsuario.apellido,
+                    email = objUsuario.email,
+                    contrasena = objUsuario.contrasena,
+                    activo = objUsuario.activo
+                };
+                response.value = objUsuarioModel;
+            }
+            catch (Exception ex)
+            {
+                response.error = true;
+                response.errorMessage = ex.Message;
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IHttpActionResult Registrar(UsuarioModel objUsuarioModel) {
+            var response = new Response<UsuarioModel>();
+            try
+            {
+                var objUsuarioBL = new UsuarioBL();
+                var objUsuario = new Usuario {
+                    nombre = objUsuarioModel.nombre,
+                    apellido = objUsuarioModel.apellido,
+                    email=objUsuarioModel.email,
+                    contrasena = objUsuarioModel.contrasena,
+                    activo = objUsuarioModel.activo,
+                    idUsuarioRegistro = objUsuarioModel.idUsuarioRegitro
+                };
+                objUsuario = objUsuarioBL.Registrar(objUsuario);
+                objUsuarioModel.idUsuario = objUsuario.idUsuario;
+                response.value = objUsuarioModel;
+            }
+            catch (Exception ex)
+            {
+                response.error = true;
+                response.errorMessage = ex.Message;
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        [Route("modificar")]
+        public IHttpActionResult Modificar(UsuarioModel objUsuarioModel) {
+            var response = new Response<bool>();
+            try
+            {
+                var objUsuarioBL = new UsuarioBL();
+                var objUsuario = new Usuario
+                {
+                    idUsuario = objUsuarioModel.idUsuario,
+                    nombre = objUsuarioModel.nombre,
+                    apellido = objUsuarioModel.apellido,
+                    email = objUsuarioModel.email,
+                    contrasena = objUsuarioModel.contrasena,
+                    activo = objUsuarioModel.activo,
+                    idUsuarioModifico = objUsuarioModel.idUsuarioRegitro
+                };
+                response.value = objUsuarioBL.Modificar(objUsuario);
+            }
+            catch (Exception ex)
+            {
+                response.error = true;
+                response.errorMessage = ex.Message;
+            }
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{idUsuario}")]
+        public IHttpActionResult Eliminar(int idUsuario)
+        {
+            var response = new Response<bool>();
+            try
+            {
+                var objUsuarioBL = new UsuarioBL();
+                response.value = objUsuarioBL.Eliminar(idUsuario);
+            }
+            catch (Exception ex)
+            {
+                response.error = true;
+                response.errorMessage = ex.Message;
+            }
+
+            return Ok(response);
+        }
+    }
+}
---
+++ UPC.PiggySave.REST/Models/UsuarioModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UPC.PiggySave.REST.Models
+{
+    public class UsuarioModel
+    {
+        public int idUsuario { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
+        public string contrasena { get; set; }
+        public int idUsuarioRegitro { get; set; }
+        public bool activo { get; set; }
+    }
+}
